c631f479bb98d95d33e3dd928700e972c6b381d8
2DAW <valeria.trujillop18@gmail.com>
2024-11-05 16:06:54 +0100

    Creada la entidad Servicio

---
+++ src/main/java/es/iesjandula/proyectocitas/model/Servicio.java
@@ -0,0 +1,74 @@
+package es.iesjandula.proyectocitas.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Servicio {
+
+    @OneToMany(mappedBy = "servicio")
+    private List<Cita> citas;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idServicio;
+    private String nombre;
+    private String descripcion;
+    private Integer duracion;
+
+    public Long getIdServicio() {
+        return idServicio;
+    }
+
+    public void setIdServicio(Long idServicio) {
+        this.idServicio = idServicio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
+
+    @Override
+    public String toString() {
+        return "Servicio{" +
+                "idServicio=" + idServicio +
+                ", nombre='" + nombre + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", duracion=" + duracion +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Servicio servicio)) return false;
+        return Objects.equals(idServicio, servicio.idServicio) && Objects.equals(nombre, servicio.nombre) && Objects.equals(descripcion, servicio.descripcion) && Objects.equals(duracion, servicio.duracion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(idServicio, nombre, descripcion, duracion);
+    }
+}
