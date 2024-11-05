69a21bac1c5bc29aede5978e07493ebd0e122d0d
mfercob430 <mfercob430@g.educaand.es>
2024-11-05 15:19:59 +0100

    Definitivo ultra 4k

---
+++ src/test/java/es/iesjandula/proyectocitas/ProyectoCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.iesjandula.proyectocitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoCitasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
