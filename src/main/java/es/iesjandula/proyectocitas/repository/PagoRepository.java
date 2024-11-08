1718a45486880822391979f042a47235a32e1e2d
mfercob430 <mfercob430@g.educaand.es>
2024-11-08 18:28:18 +0100

    Repositorio Pago y modificado el HorarioRepository

--- src/main/java/es/iesjandula/proyectocitas/repository/PagoRepository.java
+++ src/main/java/es/iesjandula/proyectocitas/repository/PagoRepository.java
@@ -1,9 +1,9 @@
 package es.iesjandula.proyectocitas.repository;
 
-import es.iesjandula.proyectocitas.model.Horario;
+import es.iesjandula.proyectocitas.model.Pago;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HorarioRepository extends JpaRepository<Horario,Long > {
+public interface PagoRepository extends JpaRepository<Pago, Long> {
 }
