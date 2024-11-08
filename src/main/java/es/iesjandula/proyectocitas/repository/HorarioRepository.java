7325f5f6cef269362d4f7d5b556a48edc8ef24ea
mfercob430 <mfercob430@g.educaand.es>
2024-11-08 18:20:23 +0100

    Repositorio Horario

---
+++ src/main/java/es/iesjandula/proyectocitas/repository/HorarioRepository.java
@@ -0,0 +1,8 @@
+package es.iesjandula.proyectocitas.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HorarioRepository extends JpaRepository {
+}
