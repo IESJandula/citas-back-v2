76371edd93e1071bf1b021cec856d60587257991
PabloGrAl <116663469+PabloGrAl@users.noreply.github.com>
2024-11-14 18:25:35 +0100

    errores solucionados

--- src/main/java/es/iesjandula/proyectocitas/repository/CitaRepository.java
+++ src/main/java/es/iesjandula/proyectocitas/repository/CitaRepository.java
@@ -4,6 +4,14 @@ import es.iesjandula.proyectocitas.model.Cita;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface CitaRepository extends JpaRepository<Cita, Long> {
+    // Método para verificar si una cita ya existe en un horario específico
+    boolean existsByFecha_hora_Cita(LocalDateTime fechaHora);
+
+    // Método para obtener citas por fecha
+    List<Cita> findByFecha_hora_CitaBetween(LocalDateTime inicio, LocalDateTime fin);
 }
